6e3463d9c306a2e707bc4a93e989f8755dc49d4f
LuckyRosario <p.rosario684@gmail.com>
2021-02-26 20:48:09 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 export function Home() {
+	let list = ["make the bed", "wash my hands", "walk the dog"];
+	const [todoList, updateTodos] = useState(list);
+	let addTask = e => {
+		console.log(e);
+		if (e.keyCode == 13) {
+			let userTask = e.target.value;
+			let newList = [...todoList, userTask];
+			updateTodos(newList);
+			e.target.value = "";
+		}
+	};
+	let deleteTask = i => {
+		let updatedList = todoList.filter((element, index) => index !== i);
+		updateTodos(updatedList);
+	};
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="container">
+				<h1>todos</h1>
+
+				<div className="todo-list">
+					<input
+						type="text"
+						name="task"
+						placeholder={
+							todoList.length == 0
+								? "No tasks, add a task"
+								: "what needs to be done?"
+						}
+						onKeyDown={addTask}
+					/>
+					<ul>
+						{todoList.map((todo, index) => (
+							<li className="todo-item" key={index}>
+								{todo}{" "}
+								<button
+									className="delete"
+									onClick={() => deleteTask(index)}>
+									x
+								</button>
+							</li>
+						))}
+					</ul>
+					<p> {todoList.length} items left</p>
+				</div>
+			</div>
 		</div>
 	);
 }
