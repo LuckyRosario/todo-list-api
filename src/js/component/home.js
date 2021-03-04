be06a03d69a1acbc7d264310ee465093250d20db
LuckyRosario <p.rosario684@gmail.com>
2021-03-04 23:31:00 +0000

    third commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,18 +10,30 @@ export function Home() {
 	const [task, setTask] = useState("");
 	const [tasks, setTasks] = useState([]);
 
-	let addTask = e => {
-		if (e.keyCode == 13) {
-			let userTask = e.target.value;
-			let newList = [...tasks, userTask];
-			setTasks(newList);
-			e.target.value = "";
-		}
-	};
+	//	let addTask = e => {
+	//		if (e.keyCode == 13) {
+	//			let userTask = e.target.value;
+	//			let newList = [...tasks, userTask];
+	//			setTasks(newList);
+	//			e.target.value = "";
+	//		}
+	//	};
 	let deleteTask = i => {
 		let updatedList = tasks.filter((element, index) => index !== i);
-		setTasks(updatedList);
+		fetch(url, {
+			method: "put",
+			body: JSON.stringify(updatedList),
+			headers: {
+				"Content-type": "application/json"
+			}
+		})
+			.then(() => getTodos())
+			.catch(error => console.log(error));
 	};
+	// 	let deleteTask = () => {
+	// 	let newTasks = tasks.filter(task => task.id != id);
+	// 	this.set;
+	// };
 	const getTodos = () => {
 		fetch(url)
 			.then(resp => {
@@ -38,11 +50,12 @@ export function Home() {
 		getTodos();
 	}, []);
 
-	let addTask = () => {
-		e.preventDefault();
+	const addTask = e => {
+		// e.preventDefault();
+		// if (e.keyCode === 13) {
 		const newTask = { label: task, done: false };
 		const updatedList = [...tasks, newTask];
-
+		console.log(e);
 		fetch(url, {
 			method: "put",
 			body: JSON.stringify(updatedList),
@@ -50,22 +63,18 @@ export function Home() {
 				"Content-type": "application/json"
 			}
 		})
-			.then(resp => resp.json())
-			.then(data => {
-				console.log(data);
-			})
+			.then(() => getTodos())
 			.catch(error => console.log(error));
+		// }
 	};
-	let deleteTask = () => {
-		let newTasks = tasks.filter(task => task.id != id);
-		this.set;
-	};
+
 	return (
 		<div>
 			<div className="container">
 				<h1>todos</h1>
 
-				<div className="todo-list" onSubmit={e => addTask(e)}>
+				{/* <div className="todo-list" onSubmit={e => addTask(e)}> */}
+				<div className="todo-list">
 					<input
 						type="text"
 						onChange={e => setTask(e.target.value)}
@@ -75,7 +84,13 @@ export function Home() {
 								? "No tasks, add a task"
 								: "what needs to be done?"
 						}
-						onKeyDown={addTask}
+						value={task}
+						onKeyUp={e => {
+							//listen to the key up and wait for the return key to be pressed (Key === 'Enter')
+							if (e.key === "Enter") {
+								addTask();
+							}
+						}}
 					/>
 					<ul>
 						{tasks.map((todo, index) => (
